1ba56365919ec8c6cdbc68b5a823b04097499f0d
ramzi05 <iseultiple@gmail.com>
2025-02-02 10:09:14 +0100

    Fix Vercel deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
+  adapter: vercel(),
   i18n: {
     defaultLocale: 'fr',
     locales: ['fr', 'ar', 'en'],
