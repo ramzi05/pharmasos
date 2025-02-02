8936d8a28efe8bad23eaca337490b1ad5d0af41b
ramzi05 <iseultiple@gmail.com>
2025-02-02 15:24:04 +0100

    Fix index page language handling using shared constants

--- dist/$server_build/chunks/astro_C8hPCczo.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_BzzarInc.mjs';
-import 'clsx';
--- dist/$server_build/chunks/languages_Bn8xGCxr.mjs
+++
@@ -1,4 +0,0 @@
-const validLanguages = ["en", "fr", "ar"];
-const defaultLanguage = "fr";
-
-export { defaultLanguage as d, validLanguages as v };
